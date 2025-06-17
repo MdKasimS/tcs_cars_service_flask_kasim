79bb49187916ac9ea16dec03a9adc15841bf8a1f
KasimSache <45384577+MdKasimS@users.noreply.github.com>
2025-06-17 23:45:43 +0530

    CORS added

--- application.py
+++ application.py
@@ -1,8 +1,11 @@
 from flask import Flask
 from config import Config
 from models.Database import db
+from flask_cors import CORS
+
 
 app = Flask(__name__)
+CORS(app)
 app.config.from_object(Config)
 
 # Initialize the database with the app
